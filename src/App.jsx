ca1c3ba78f80a8e3bc60dba9ba7c94ef88c60c0e
Jaylan Snelson <jaylan210@gmail.com>
2023-09-12 16:37:38 -0500

    Updating README.md and removing unecessary dependecies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Button1 from './components/Button1'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <h1>Component Library Collection</h1>
         <p>Storybook Link coming soon.</p>
-        <Button1 />
+        <p>This is a componet library built using both Custom CSS and tailwind CSS.</p>
       </div>
     </>
   )
