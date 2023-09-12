b5c0a563dbbe35564dbec2a53b6e0251b57f8acc
Jaylan Snelson <jaylan210@gmail.com>
2023-09-12 15:45:12 -0500

    Cleaing up storybook. Next is to build to make sure it works

---
+++ src/components/Button1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button1() {
+  return (
+    <div>Button1</div>
+  )
+}
---
+++ src/stories/Button1.stories.js
@@ -0,0 +1,9 @@
+import Button1 from "../components/Button1";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Buttons/Button 1',
+  component: Button1,
+};
+
+export const Primary = {};
