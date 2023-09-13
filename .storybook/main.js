0d3e5ad56baaf53099520d48149ca8d46020e951
Jaylan Snelson <jaylan210@gmail.com>
2023-09-13 14:02:03 -0500

    Adding A11y for accescibiiltiy

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/preset-scss",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
