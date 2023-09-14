0913720a21aa871ba8dc2de48bb4c2a4a9c8c5e0
Jaylan Snelson <jaylan210@gmail.com>
2023-09-13 19:03:55 -0500

    updating deploy to run storybook instead. Still catn seem to get vite to build. Maybe ill have better luck with storybook

---
+++ storybook-static/assets/preview-e804171e.js
@@ -0,0 +1,2 @@
+const a={parameters:{backgrounds:{default:"light-primary",values:[{name:"light-primary",value:"#ddd"},{name:"dark-primary",value:"#000"}]},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{a as default};
+//# sourceMappingURL=preview-e804171e.js.map
