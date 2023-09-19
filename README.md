b0ca2061517e93bc26cb240146466417d122d0f1
Jaylan Snelson <jaylan210@gmail.com>
2023-09-18 21:24:12 -0500

    Adding icons for twitter and github. Updating homepage fonts. Adding animation for storybook link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Component Library Collection
 
-Storybook Link: Coming Soon
+Storybook Link: https://main--6508db59c01ba56d8f28e200.chromatic.com/
 Homepage: https://jaysnel.github.io/component-library-react/
 
 This is a componet library built with pure CSS.
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 import ButtonList from './components/Buttons'
+import TwitterIcon from './assets/twitterIcon'
+import GithubIcon from './assets/githubIcon'
 
 function App() {
+  const navigateToSocial = (social) => {
+    const socialMapping = {
+      twitter: 'https://twitter.com/_jsnelson_',
+      github: 'https://github.com/jaysnel/component-library-react',
+    }
+    window.open(socialMapping[social], '_blank', 'noopener,noreferrer');
+  }
+
   return (
     <>
-      <div>
+      <div className='home-page'>
         <h1>Component Library Collection</h1>
+        <p>This is a component library built using pure CSS.</p>
+        <div className='storybook-link'>
         <a href="https://main--6508db59c01ba56d8f28e200.chromatic.com/" target="_blank">View Storybook</a>
-        <p>This is a componet library built using both Custom CSS and tailwind CSS.</p>
-        <ButtonList />
+        </div>
+        <div className='social-icons'>
+          <TwitterIcon
+          navigationFunction={navigateToSocial}/>
+          <GithubIcon 
+          navigationFunction={navigateToSocial}/>
+        </div>
+        {/* <ButtonList /> */}
       </div>
     </>
   )
